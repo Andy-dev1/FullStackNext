ec52cb110286f07fb246fa4ecb0a02929c9639ec
Andy-dev1 <andre.andre.jazz@gmail.com>
2024-03-10 01:44:22 -0300

    Components

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,12 @@
+import Navbar from "@/components/navbar/Navbar";
 import styles from "./homepage.module.css";
+import Link from "next/link";
+import Footer from "@/components/footer/Footer";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      
+    </div>
+  );
 }
