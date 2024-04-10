0cd61b27c32695bac0e5d21df24a30bc38008d99
Andy-dev1 <andre.andre.jazz@gmail.com>
2024-04-10 18:16:28 -0300

    Categories posts

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,27 +2,24 @@ import Link from "next/link";
 import styles from "./card.module.css";
 import Image from "next/image";
 
-const Card = () => {
+const Card = ({ key, item }) => {
   return (
-    <div className={styles.container}>
+    <div className={styles.container} key={key}>
       <div className={styles.imageContainer}>
-       <Image src="/p1.jpeg" alt="" fill className={styles.image}/>
+        <Image src="/p1.jpeg" alt="" fill className={styles.image} />
       </div>
       <div className={styles.textContainer}>
         <div className={styles.detail}>
-          <span className={styles.date}>11.02.2024 - </span>
-          <span className={styles.category}>CULTURE</span>
+          <span className={styles.date}>{item.createdAt.substring(0,10)} - </span>
+          <span className={styles.category}>{item.catSlug}</span>
         </div>
         <Link href="/">
-          <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+          <h1>{item.title}</h1>
         </Link>
-        <p className={styles.desc}>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam vel
-          officia quas, culpa dicta reprehenderit. Dolor vel eveniet iure,
-          possimus nihil modi ipsum voluptates quidem, aspernatur optio rem
-          fugit accusamus?
-        </p>
-        <Link href="/" className={styles.link}>Read More</Link>
+        <p className={styles.desc}>{item.desc}</p>
+        <Link href="/" className={styles.link}>
+          Read More
+        </Link>
       </div>
     </div>
   );
