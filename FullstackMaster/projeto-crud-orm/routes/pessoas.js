44835d4f8612da40f02ebb36fe9f9d45c6d6ccc3
Andy-dev1 <andre.andre.jazz@gmail.com>
2024-05-07 00:16:16 -0300

    orm project

---
+++ FullstackMaster/projeto-crud-orm/routes/pessoas.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const pessoasController = require("../controllers/pessoas");
+const router = express.Router();
+const model = require("../models/index")
+
+router.get("/", pessoasController.index.bind(null,model.models));
+
+module.exports = router;
